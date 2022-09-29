7fc894abe2826e1377946d0b02b1437b7c75488e
PrafulTelgote <prafultelgote4@gmail.com>
2022-09-29 21:14:26 +0530

    update the driver, cab Services, Driver Services impl

--- CabBookingApp/src/main/java/com/cab/models/Driver.java
+++ CabBookingApp/src/main/java/com/cab/models/Driver.java
@@ -1,10 +1,19 @@
 package com.cab.models;
 
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Driver extends BasicDetails {
 	
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer driverId;
 	private Integer licenceNo;
+	@Embedded
 	private Cab cab;
 	private Float rating;
 
