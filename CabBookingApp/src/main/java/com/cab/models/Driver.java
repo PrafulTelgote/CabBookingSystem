153adabaf1912ff3a005bd12109eac24c71f2ebf
PrafulTelgote <prafultelgote4@gmail.com>
2022-09-29 22:16:18 +0530

    Create Driver entity class and there respectively classes

--- CabBookingApp/src/main/java/com/cab/models/Driver.java
+++ CabBookingApp/src/main/java/com/cab/models/Driver.java
@@ -1,10 +1,20 @@
 package com.cab.models;
 
+import java.util.*;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Driver extends BasicDetails {
@@ -12,9 +22,83 @@ public class Driver extends BasicDetails {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer driverId;
-	private Integer licenceNo;
-	@Embedded
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	
+	@JoinColumn(name = "cabId")
+	
+	@JsonIgnore
 	private Cab cab;
-	private Float rating;
+	
+	@OneToMany(cascade = CascadeType.ALL,mappedBy = "cabDriver",orphanRemoval = true)
+	@JsonIgnore
+	List<TripBooking> tripDetailsList = new ArrayList<>();
+	
+	@NotNull(message = "License Number cannot be null")
+	@Column(unique = true)
+	private String licenseNumber;
+	
+	private Boolean availablity = true;
+	
+	/*Getter Setter Methods Start*/
+
+	public Integer getDriverId() {
+		return driverId;
+	}
+
+	public void setDriverId(Integer driverId) {
+		this.driverId = driverId;
+	}
+
+	public Cab getCab() {
+		return cab;
+	}
+
+	public void setCab(Cab cab) {
+		this.cab = cab;
+	}
+
+	public List<TripBooking> getTripDetailsList() {
+		return tripDetailsList;
+	}
+
+	public void setTripDetailsList(List<TripBooking> tripDetailsList) {
+		this.tripDetailsList = tripDetailsList;
+	}
+
+	public String getLicenseNumber() {
+		return licenseNumber;
+	}
+
+	public void setLicenseNumber(String licenseNumber) {
+		this.licenseNumber = licenseNumber;
+	}
+
+	public Boolean getAvailablity() {
+		return availablity;
+	}
+
+	public void setAvailablity(Boolean availablity) {
+		this.availablity = availablity;
+	}
+	
+	/*Getter Setter Methods Ended*/
+	
+	
+	/*override method*/
+
+	@Override
+	public String toString() {
+		return "Driver [driverId=" + driverId + ", cab=" + cab + ", tripDetailsList=" + tripDetailsList
+				+ ", licenseNumber=" + licenseNumber + ", availablity=" + availablity + "]";
+	}
+	
+
+	
+	
+	
+	
+	
+	
 
 }
---
+++ CabBookingApp/src/main/java/com/cab/models/DriverCabDTO.java
@@ -0,0 +1,91 @@
+package com.cab.models;
+
+public class DriverCabDTO {
+	
+	
+private String licenseNumber;
+	
+	private String numberPlate;
+	
+	private Float ratePerKms;
+	
+	private String carType;
+
+	/**
+	 * @return the licenseNumber
+	 */
+	public String getLicenseNumber() {
+		return licenseNumber;
+	}
+
+	/**
+	 * @param licenseNumber the licenseNumber to set
+	 */
+	public void setLicenseNumber(String licenseNumber) {
+		this.licenseNumber = licenseNumber;
+	}
+
+	/**
+	 * @return the numberPlate
+	 */
+	public String getNumberPlate() {
+		return numberPlate;
+	}
+
+	/**
+	 * @param numberPlate the numberPlate to set
+	 */
+	public void setNumberPlate(String numberPlate) {
+		this.numberPlate = numberPlate;
+	}
+
+	/**
+	 * @return the ratePerKms
+	 */
+	public Float getRatePerKms() {
+		return ratePerKms;
+	}
+
+	/**
+	 * @param ratePerKms the ratePerKms to set
+	 */
+	public void setRatePerKms(Float ratePerKms) {
+		this.ratePerKms = ratePerKms;
+	}
+
+	/**
+	 * @return the carType
+	 */
+	public String getCarType() {
+		return carType;
+	}
+
+	/**
+	 * @param carType the carType to set
+	 */
+	public void setCarType(String carType) {
+		this.carType = carType;
+	}
+
+	@Override
+	public String toString() {
+		return "DriverCabDTO [licenseNumber=" + licenseNumber + ", numberPlate=" + numberPlate + ", ratePerKms="
+				+ ratePerKms + ", carType=" + carType + "]";
+	}
+
+	public DriverCabDTO(String licenseNumber, String numberPlate, Float ratePerKms, String carType) {
+		super();
+		this.licenseNumber = licenseNumber;
+		this.numberPlate = numberPlate;
+		this.ratePerKms = ratePerKms;
+		this.carType = carType;
+	}
+
+	public DriverCabDTO() {
+	}
+	
+	
+	
+	
+
+}
