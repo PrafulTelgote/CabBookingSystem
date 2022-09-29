b995853e15966e0c471061872b7f86194134d0b6
akbora1994 <borasteak877@gmail.com>
2022-09-29 22:30:54 +0530

    entity classes added

--- CabBookingApp/src/main/java/com/cab/models/TripBooking.java
+++ CabBookingApp/src/main/java/com/cab/models/TripBooking.java
@@ -1,25 +1,46 @@
 package com.cab.models;
-
-import java.time.LocalDateTime;
-
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class TripBooking {
 	
-	
+
+	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer trimBookingId;
+	
 	private Integer customerId;
+	
+	@ManyToOne
+	@JoinColumn(name = "cabDriverId")
 	private Driver driver;
+	
+	@NotNull(message = "Pickup Location cannot be null")
 	private String fromLocation;
+	
+	@NotNull(message = "Drop Location cannot be null")
 	private String toLocation;
+	
+
+	
 	private LocalDateTime fromDateTime;
+	
+	
 	private LocalDateTime toDateTime;
-	private Boolean status;
+	
+	private Boolean status = false;
+	
+	@NotNull(message = "Distance cannot be null")
 	private Float distanceInKm;
+	
 	private Float bill;
 
 }
